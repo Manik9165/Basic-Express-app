08ecb19c7755871269676736aedfe7caf92c62ea
manikkapoor <manik.kapoor@tungsten-network.com>
2024-09-14 22:26:17 +0530

    Added functionality to add a note

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 This is a basic express app project built while learning express.
 
 In this project, user can create tasks and those tasks will be displayed on the UI.
+We will be using File module of Node.js to create and display notes.
 
 Tech used - Node.js, Ejs, Express, Tailwind
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const file = require('fs');
 const app = express();
 
 app.use(express.json());
@@ -9,7 +10,16 @@ app.set('view engine', 'ejs');
 
 
 app.get('/', (req, res) => {
-    res.render('index');
+    file.readdir('./files', (err, files) => {
+        if (err) throw err;
+        res.render('index', { files: files });
+    })
+})
+
+app.post('/create', (req, res) => {
+    file.writeFile(`${__dirname}/files/${req.body.note.split(' ').join('')}.txt`, req.body.description, (err) => {
+        res.redirect('/');
+    });
 })
 
 app.listen(3000, () => {
